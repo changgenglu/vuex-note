7c15d7baee4c31dc78030c53248a09c7eefe5730
Ivan_Ma <ivan@rexlite.com.tw>
2023-06-21 14:31:07 +0800

    feat: 使用 mapMutations

--- src/components/ClickButton.vue
+++ src/components/ClickButton.vue
@@ -1,36 +1,32 @@
 <template>
     <div>
         <p>author: {{ author }}</p>
-        <p>Loading: {{ ifLoading }}</p>
-        <button @click="reverseLoad(), addTimes()">Reverse</button>
-        <p>Click Times: {{ clicked }}</p>
+        <p>Loading: {{ is_loading }}</p>
+        <button @click="Loaded(), addTimes()">Reverse</button>
+        <p>Click Times: {{ clicked_times }}</p>
         <button @click="resetCounter()">Reset Counter</button>
     </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapMutations, mapState } from 'vuex';
 
 export default {
     computed: {
         author() {
             return "changgenglu"
         },
-        ...mapState({
-            ifLoading: state => state.is_loading,
-            clicked: state => state.clicked_times
-        })
+        ...mapState([
+            'is_loading',
+            'clicked_times'
+        ])
     },
     methods: {
-        reverseLoad() {
-            this.$store.commit("Loaded");
-        },
-        addTimes() {
-            this.$store.commit("addTimes");
-        },
-        resetCounter() {
-            this.$store.commit("resetCounter");
-        }
+        ...mapMutations([
+            "Loaded",
+            "addTimes",
+            "resetCounter"
+        ]),
     }
 }
 </script>
