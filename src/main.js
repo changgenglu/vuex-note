edc7055f82572657a95d9650fd92f23c50715c10
Ivan_Ma <ivan@rexlite.com.tw>
2023-06-21 11:08:47 +0800

    feat(main.js): 將 vuex 加入 main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
-// import store from "./store";
+import store from "./store";
 
-createApp(App).mount("#app");
+new Vue({
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
