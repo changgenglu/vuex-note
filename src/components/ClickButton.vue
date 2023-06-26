1256b27f736bc53b465d5bae07a0363050ca41b3
Ivan_Ma <ivan@rexlite.com.tw>
2023-06-26 14:05:40 +0800

    feat: 新增取得五筆資料中，為女性的資料有幾筆

--- src/components/ClickButton.vue
+++ src/components/ClickButton.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <p>author: {{ author }}</p>
-        <p>random user: {{ user_data }}</p>
-        <button @click="Reload()">get user data</button>
+        <p>random user: {{ FemaleNumber }}</p>
+        <button @click="Reload()">get female number</button>
         <p>{{ is_loading }}</p>
-        <button @click="Loaded(), addTimes(6)">Reverse</button>
+        <button @click="dataLoading(), addTimes(6)">Reverse</button>
         <p>Click Times: {{ clicked_times }}</p>
         <button @click="resetCounter()">Reset Counter</button>
     </div>
@@ -22,11 +22,15 @@ export default {
             'user_data',
             'is_loading',
             'clicked_times'
-        ])
+        ]),
+        FemaleNumber() {
+            console.log(this.user_data);
+            return this.user_data.filter(item => item.gender == 'female').length;
+        }
     },
     methods: {
         ...mapMutations([
-            "Loaded",
+            "dataLoading",
             "resetCounter"
         ]),
         ...mapActions(["get_user", "clicked_actions"]),
@@ -34,7 +38,7 @@ export default {
             this.clicked_actions(1);
         },
         Reload() {
-            this.$store.commit("initLoading");
+            this.$store.commit("dataLoading");
             this.get_user();
         }
     },
